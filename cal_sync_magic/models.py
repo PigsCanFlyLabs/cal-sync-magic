eb347e36e52614f2d82a4f2c81355f9e61a2bde2
Holden Karau <holden@pigscanfly.ca>
2023-02-16 17:21:54 -0800

    oops move g account back

--- cal_sync_magic/models.py
+++ cal_sync_magic/models.py
@@ -37,6 +37,98 @@ API_SERVICE_NAME = "calendar"
 API_VERSION = "v3"
 
 
+class GoogleAccount(models.Model):
+    account_id = models.AutoField(primary_key=True, null=False)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    google_user_email = models.CharField(max_length=250, null=False)
+    credentials = models.CharField(max_length=5000, null=False)
+    credential_expiry = models.DateTimeField(null=True)
+    last_refreshed = models.DateTimeField(default=datetime.now)
+    unique_together = ["user", "google_user_email"]
+    calendar_sync_enabled = models.BooleanField(default=True)
+    second_chance_email = models.BooleanField(default=False)
+    more_spam_filter = models.BooleanField(default=False)
+    delete_events_from_email = models.BooleanField(default=False)
+
+    @property
+    def scopes(self):
+        creds = self.get_credentials()
+        return creds.scopes
+
+    def get_friendly_scopes(self):
+        friendly_scopes = []
+        try:
+            account_scopes = self.scopes
+            for scope_name in scopes.keys():
+                scope_values = scopes[scope_name]
+                if all(map(lambda scope: scope in account_scopes, scope_values)):
+                    friendly_scopes.append(scope_name)
+        except:
+            return ["Account re-add required."]
+        return friendly_scopes
+
+    def get_credentials(self):
+        """Get the credentials, try and refresh if needed, and if we can't refresh.
+        delete the credentials so we can trigger a re-add."""
+        if self.credentials is None:
+            return None
+        stored_creds = json.loads(self.credentials)
+        # Get expirery so we can figure out if we need a refresh
+        if self.credential_expiry is not None:
+            stored_creds["expiry"] = self.credential_expiry
+        else:
+            stored_creds["expiry"] = datetime.now()
+
+        # Drop timezone info
+        stored_creds["expiry"] = stored_creds["expiry"].replace(tzinfo=None)
+        user_credentials = google.oauth2.credentials.Credentials(**stored_creds)
+        try:
+            if user_credentials.expired:
+                http_request = google.auth.transport.requests.Request()
+                user_credentials.refresh(http_request)
+        except exceptions.RefreshError:
+              revoke = requests.post(
+                  'https://oauth2.googleapis.com/revoke',
+                  params={'token': user_credentials.token},
+                  headers = {'content-type': 'application/x-www-form-urlencoded'})
+              self.credentials = None
+              self.save()
+              return None
+        self.credentials = user_credentials.to_json()
+        self.credential_expiry = user_credentials.expiry
+        self.save()
+        return user_credentials
+
+    def calendar_service(self):
+        return build(API_SERVICE_NAME, API_VERSION, credentials=self.get_credentials())
+
+    def refresh_calendars(self):
+        """Refresh the user calendars. Defined here so we can share refresh logic."""
+        current_cals = self.calendar_service().calendarList().list().execute()
+        for cal in current_cals['items']:
+            deleted = "deleted" in cal and cal["deleted"]
+            UserCalendar.objects.update_or_create(
+                user=self.user,
+                google_account=self,
+                google_calendar_id=cal['id'],
+                defaults={"deleted": deleted, "name": cal['summary']})
+
+    def get_config_form(self):
+        f = UpdateGoogleAccountForm()
+        f.fields["account_id"].initial = self.account_id
+        f.fields["calendar_sync_enabled"].initial = self.calendar_sync_enabled
+        f.fields["second_chance_email"].initial = self.second_chance_email
+        f.fields["delete_events_from_email"].initial = self.delete_events_from_email
+        return f
+
+
+    class Meta:
+        app_label = "cal_sync_magic"
+
+
 class UserCalendar(models.Model):
     def hex_uuid(): # type: ignore
         """Hecking MYSQL hex UUID issue."""
